97ff9954ec1f2cd1cda7fb15a5a237baceecfce0
rgdevment <mhidalgorg@gmail.com>
2026-01-25 01:07:54 -0300

    feat: add GitHub Copilot custom instructions and update solution structure

--- CopyPaste.UI/App.xaml.cs
+++ CopyPaste.UI/App.xaml.cs
@@ -1,3 +1,21 @@
+// CopyPaste - High performance clipboard manager
+// Copyright (C) 2026 Mario Hidalgo G. (rgdevment)
+//
+// This software and all original source files in this repository are part of CopyPaste.
+//
+// CopyPaste is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// CopyPaste is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with CopyPaste. If not, see <https://github.com/rgdevment/CopyPaste/blob/main/LICENSE>.
+
 using CopyPaste.Core;
 using CopyPaste.Listener;
 using Microsoft.UI.Xaml;
