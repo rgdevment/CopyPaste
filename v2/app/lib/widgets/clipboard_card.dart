cb053c92977255f12205321f9ef2b11551de3cad
rgdevment <mhidalgorg@gmail.com>
2026-03-05 22:20:32 -0300

    fix: improve time formatting in ClipboardCard widget

--- v2/app/lib/widgets/clipboard_card.dart
+++ v2/app/lib/widgets/clipboard_card.dart
@@ -911,8 +911,9 @@ class _ClipboardCardState extends State<ClipboardCard> {
     final h = seconds ~/ 3600;
     final m = (seconds % 3600) ~/ 60;
     final s = seconds % 60;
-    if (h > 0)
+    if (h > 0) {
       return '$h:${m.toString().padLeft(2, '0')}:${s.toString().padLeft(2, '0')}';
+    }
     return '$m:${s.toString().padLeft(2, '0')}';
   }
 
