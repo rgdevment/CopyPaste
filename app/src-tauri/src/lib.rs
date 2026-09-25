07e444a4f1531b22cc683c2d3166a93cfa67cac3
rgdevment <mhidalgorg@gmail.com>
2026-09-25 10:29:45 -0300

    feat(app): the updater and the channel it will read

--- app/src-tauri/src/lib.rs
+++ app/src-tauri/src/lib.rs
@@ -12,6 +12,7 @@ pub fn run() {
         }))
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .invoke_handler(tauri::generate_handler![
             settings::settings,
